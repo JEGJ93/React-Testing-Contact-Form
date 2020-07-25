dda1faf92f34b96c0938e5d3e147118a1b22423a
Julio Gomez <jgod93@Julios-MacBook-Pro-20.local>
2020-07-25 14:55:16 -0400

    MvP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  render(
+  <App />
+  );
+});
+test("renders greeting on Greeting component", async () => {
+  // Arrange 
+  const { getByText } = render(<ContactForm />);
+  // Act
+  const contact = getByText(/First Name*/i);
+  // Assert
+  expect(contact).toBeInTheDocument();
 });
